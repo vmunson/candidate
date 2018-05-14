5b949faaccb3e9cc3699d55b89bbcea932970d05
vmunson <virginia.munson@gmail.com>
2018-05-14 16:20:11 -0400

    setup

---
+++ candidate.js
@@ -0,0 +1,34 @@
+let Candidate = [{
+    id: "one",
+    name: "joe",
+    tags: ["cool", "neat", "awesome"],
+    selected: true,
+  },{
+    id: "two",
+    name: "joe",
+    tags: ["cool", "sweet", "awesome"],
+    selected: false,
+  },{
+    id: "two",
+    name: "joe",
+    tags: ["cool", "sweet", "radical"],
+    selected: true,
+  },{
+    id: "two",
+    name: "joe",
+    tags: ["awesome", "sweet", "cool"],
+    selected: true,
+  }]
+
+function sharedTags(candidates) {
+    let tags = []
+    candidates.map((candidate)=>{
+      if(candidate.selected){
+
+      }
+    })
+}
+    
+
+sharedTags(Candidate)
+
